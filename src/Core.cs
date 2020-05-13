f353b7058f8c0fc987c4755e3ee6f722f3b48ed6
csnewcs <cs.newcs0@gmail.com>
2020-05-13 18:07:42 +0900

    서버 제작자가 잠수

--- src/Core.cs
+++ src/Core.cs
@@ -1,23 +1,47 @@
 using System;
 using System.IO;
 using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace CheckCovid19
 {
-    class UserIO
+    class User
     {
         public string download(string userID)
         {
             return "";
         }
-        public string upload(string userID, string userData)
+        public bool upload(string userID, JObject userData)
         {
-            return "";
+            string url = File.ReadAllLines("config.txt")[0];
+            WebClient client = new WebClient();
+            
+            client.Headers.Add("user-agent", "CheckCovid19");
+            try
+            {
+                client.UploadString(url + "/" + userID, "PUT", userData.ToString());
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
-        
-    }
-    class UserManage
-    {
-        
+        public string addUser(string userID, int grade, int @class, int number, string name)
+        {
+            JObject user = new JObject();
+            user.Add("grade", grade);
+            user.Add("class", @class);
+            user.Add("number", number);
+            if (upload(userID, user))
+            {
+                return $"{grade}학년 {@class}반 {number}번 {name}(ID: {userID}) 추가가 완료되었습니다.";
+            }
+            else
+            {
+                return $"추가 실패, 정보를 확인 후 다시 시도해주세요.";
+            }
+        }        
     }
 }
