e026149d023e0dea4d1a5b404288a256dd2d9a07
csnewcs <cs.newcs0@gmail.com>
2020-05-13 12:20:53 +0900

    Made Projcect

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Covid_Check_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
