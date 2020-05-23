0ec5dbfda49256f99d31219ae0882fb945004dc8
csnewcs <cs.newcs0@gmail.com>
2020-05-23 18:37:18 +0900

    자동으로 버튼이 비활성화 되지 않는 버그 수정

--- src/GUI/Program.cs
+++ src/GUI/Program.cs
@@ -120,6 +120,8 @@ namespace CovidCheckClientGui
         void checkOKClicked(object sender, EventArgs e)
         {
             string id = checkInsertID.Text;
+            checkOK.Sensitive = false;
+            checkInsertID.Text = "";
             Thread thread = new Thread(new ThreadStart(() => {check(id);}));
             thread.Start();
             checkInsertID.Text = "";
@@ -127,31 +129,39 @@ namespace CovidCheckClientGui
         void checkDoubtOKClicked(object sender, EventArgs e)
         {
             string id = checkDoubtInsertID.Text;
+            checkDoubtOK.Sensitive = false;
+            checkDoubtInsertID.Text = "";
             Thread thread = new Thread(new ThreadStart(() => {checkDoubt(id);}));
             thread.Start();
             checkDoubtInsertID.Text = "";
         }
-        async void uncheckOKClicked(object sender, EventArgs e)
+        void uncheckOKClicked(object sender, EventArgs e)
         {
-            Thread thread = new Thread(new ThreadStart(() => {uncheck(uncheckInsertID.Text);}));
+            string id = uncheckInsertID.Text;
+            uncheckOK.Sensitive = false;
+            Thread thread = new Thread(new ThreadStart(() => {uncheck(id);}));
             thread.Start();
-            await Task.Delay(10);
             uncheckInsertID.Text = "";
         }
         void insertUserClicked(object sender, EventArgs e)
         {
-            Thread thread = new Thread(new ThreadStart(() => {addUser(addIsTeacher.Active, addInsertID.Text, addInsertNumber.Text, addInsertName.Text, addInsertGrade.Text, addInsertClass.Text);}));
-            addInsertID.Sensitive = false;
-            addInsertNumber.Sensitive = false;
-            addInsertName.Sensitive = false;
-            addInsertGrade.Sensitive = false;
-            addInsertClass.Sensitive = false;
+            string[] info = new string[5] {
+                addInsertID.Text, addInsertNumber.Text, addInsertName.Text, addInsertGrade.Text, addInsertClass.Text
+            };
+            bool student = addIsTeacher.Active;
             insertUser.Sensitive = false;
+            addInsertNumber.Text = "";
+            addInsertClass.Text = "";
+            addInsertName.Text = "";
+            addInsertGrade.Text = "";
+            addInsertGrade.Text = "";
+            Thread thread = new Thread(new ThreadStart(() => {addUser(student, info[0], info[1], info[2], info[3], info[4]);}));
             thread.Start();
         }
         void delInsertUserClicked(object sender, EventArgs e)
         {
             string id = delInsertID.Text;
+            delInsertUser.Sensitive = false;
             Thread thread = new Thread(new ThreadStart(() => {delUser(id);}));
             thread.Start();
             delInsertID.Text = "";
@@ -163,6 +173,7 @@ namespace CovidCheckClientGui
                 delInsertClass.Text,
                 delInsertNumber.Text
             };
+            delInsertUserWithoutID.Sensitive = false;
             Thread thread = new Thread(new ThreadStart(() => {delUser(info[0], info[1], info[2]);}));
             thread.Start();
             delInsertGrade.Text = "";
@@ -173,12 +184,15 @@ namespace CovidCheckClientGui
         //ID 없이 입력하는 버튼이 눌렸을 때 실행되는 이벤트
         void checkInsertUserClicked(object sender, EventArgs e)
         {
-            Thread thread = new Thread(new ThreadStart(() => {check(checkInsertGrade.Text, checkInsertClass.Text, checkInsertNumber.Text);}));
-            thread.Start();
-            checkInsertGrade.Sensitive = false;
-            checkInsertClass.Sensitive = false;
-            checkInsertNumber.Sensitive = false;
+            string[] info = new string[3] {
+                checkInsertGrade.Text, checkInsertClass.Text, checkInsertNumber.Text
+            };
             checkInsertUser.Sensitive = false;
+            checkInsertGrade.Text = "";
+            checkInsertClass.Text = "";
+            checkInsertNumber.Text = "";
+            Thread thread = new Thread(new ThreadStart(() => {check(info[0], info[1], info[2]);}));
+            thread.Start();
         }
         void checkDoubtInsertUserClicked(object sender, EventArgs e)
         {
@@ -197,11 +211,14 @@ namespace CovidCheckClientGui
         }
         void uncheckInsertUserClicked(object sender, EventArgs e)
         {
-            Thread thread = new Thread(new ThreadStart(() => {uncheck(uncheckInsertGrade.Text, uncheckInsertClass.Text, uncheckInsertNumber.Text);}));
+            string[] info = new string[3] {
+                uncheckInsertGrade.Text, uncheckInsertClass.Text, uncheckInsertNumber.Text
+            };
+            Thread thread = new Thread(new ThreadStart(() => {uncheck(info[0], info[1], info[2]);}));
             thread.Start();
-            uncheckInsertGrade.Sensitive = false;
-            uncheckInsertClass.Sensitive = false;
-            uncheckInsertNumber.Sensitive = false;
+            uncheckInsertGrade.Text = "";
+            uncheckInsertClass.Text = "";
+            uncheckInsertNumber.Text = "";
             uncheckInsertUser.Sensitive = false;
         }
         
@@ -273,12 +290,6 @@ namespace CovidCheckClientGui
             }
             Application.Invoke (delegate {
                 addLog(toLog);
-                checkInsertGrade.Sensitive = !checkIsTeacher.Active;
-                checkInsertClass.Sensitive = !checkIsTeacher.Active;
-                checkInsertNumber.Sensitive = true;
-                checkInsertGrade.Text = "";
-                checkInsertClass.Text = "";
-                checkInsertNumber.Text = "";
             });
         }
         void checkDoubt(string id)
@@ -413,12 +424,6 @@ namespace CovidCheckClientGui
             }
             Application.Invoke (delegate {
                 addLog(toLog);
-                uncheckInsertGrade.Sensitive = !uncheckIsTeacher.Active;
-                uncheckInsertClass.Sensitive = !uncheckIsTeacher.Active;
-                uncheckInsertNumber.Sensitive = true;
-                uncheckInsertGrade.Text = "";
-                uncheckInsertClass.Text = "";
-                uncheckInsertNumber.Text = "";
             });
         }
         void addUser(bool isNotStudent, string id, string number, string name, string grade, string @class)
