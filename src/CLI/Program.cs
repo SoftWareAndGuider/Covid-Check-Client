22f2bdef373445da3af2bae31d171512c79eea7f
csnewcs <cs.newcs0@gmail.com>
2020-05-30 10:58:32 +0900

    정보 미입력시 다시 입력하도록 업데이트

--- src/CLI/Program.cs
+++ src/CLI/Program.cs
@@ -21,21 +21,24 @@ namespace Covid_Check_Client
                         change = program.check();
                         break;
                     case "2":
-                        change = program.uncheck();
+                        change = program.ondoCheck();
                         break;
                     case "3":
-                        change = program.add();
+                        change = program.uncheck();
                         break;
                     case "4":
-                        change = program.remove();
+                        change = program.add();
                         break;
                     case "5":
-                        change = program.checkWithoutID();
+                        change = program.remove();
                         break;
                     case "6":
+                        change = program.checkWithoutID();
+                        break;
+                    case "8":
                         change = program.uncheckWithoutID();
                         break;
-                    case "7":
+                    case "9":
                         change = program.removeWithoutID();
                         break;
                     default:
@@ -51,16 +54,8 @@ namespace Covid_Check_Client
             string change = "0";
             while (true)
             {
-                Console.WriteLine("현재는 체크모드 입니다. 모드를 변경하려면 change를, 프로그램 종료는 exit를 입력해 주세요\n체크하려면 사용자의 바코드를 스캔하거나 숫자를 입력하세요.");
-                string scan = Console.ReadLine();
-
-                if (scan == "change") //모드 바꾸기
-                {
-                    change = changeMode();
-                    break;
-                }
-                else if (scan == "exit") Environment.Exit(0);
-                JObject result = user.check(scan);
+                if (first("체크", out change, "체크하려면 사용자의 바코드를 스캔하거나 숫자를 입력하세요.")) return change;
+                JObject result = user.check(change);
                 if ((bool)result["success"])
                 {
                     Console.WriteLine($"{result["data"]["name"]}(ID: {result["data"]["id"]})의 체크가 완료되었습니다.\n");
@@ -70,7 +65,6 @@ namespace Covid_Check_Client
                     Console.WriteLine("체크를 실패하였습니다. 확인 후 다시 시도해주세요\n");
                 }
             }
-            return change;
         }
         string uncheck()
         {
@@ -138,15 +132,34 @@ namespace Covid_Check_Client
                 Console.WriteLine();
             }
         }
+        string ondoCheck()
+        {
+            User user = new User();
+            while (true)
+            {
+                string scan = "";
+                if (first("삭제", out scan, "사용자를 발열 체크하려면 사용자의 바코드를 스캔하거나 숫자를 입력하세요.")) return scan; //모드 바꾸기
+                JObject result = user.check(scan, true);
+                if ((bool)result["success"])
+                {
+                    Console.WriteLine($"{result["data"]["name"]}(ID: {result["data"]["id"]})의 발열 체크가 완료되었습니다.\n");
+                }
+                else
+                {
+                    Console.WriteLine("발열 체크를 실패하였습니다. 확인 후 다시 시도해주세요\n");
+                }
+                Console.WriteLine();
+            }
+        }
         string changeMode()
         {
             string change = "";
             while (true)
             {
-                Console.WriteLine("1: 사용자 체크\n2: 사용자 체크 해제\n3: 사용자 추가\n4: 사용자 삭제\n5: ID없이 사용자 체크\n6: ID없이 사용자 체크 해제\n7: ID없이 사용자 삭제");
+                Console.WriteLine("1: 사용자 체크\n2: 사용자 발열 체크\n3: 사용자 체크 해제\n4: 사용자 추가\n5: 사용자 삭제\n6: ID없이 사용자 체크\n7: ID없이 사용자 발열 체크\n8: ID없이 사용자 체크 해제\n9: ID없이 사용자 삭제");
                 change = Console.ReadLine();
                 string[] lists = new string[] {
-                    "1", "2", "3", "4", "5", "6", "7" 
+                    "1", "2", "3", "4", "5", "6", "7", "8", "9"
                 };
                 if (lists.Contains(change)) break;
                 Console.WriteLine("올바른 번호를 선택해 주세요");
@@ -227,8 +240,17 @@ namespace Covid_Check_Client
         bool first(string title, out string what, string and = "")
         {
             Console.WriteLine($"현재는 사용자 {title}모드 입니다. 모드를 변경하려면 change를, 프로그램 종료는 exit를 입력해 주세요\n{and}");
-            what = Console.ReadLine();
             bool turn = false;
+            while (true)
+            {
+                what = Console.ReadLine();
+                if (string.IsNullOrEmpty(what))
+                {
+                    Console.WriteLine("정보를 입력하지 않았습니다. 다시 입력해주세요.");
+                    continue;
+                }
+                break;
+            }
             if (what == "change") //모드 바꾸기
             {
                 what = changeMode();
@@ -242,9 +264,27 @@ namespace Covid_Check_Client
         {
             string[] info = new string[3];
             Console.WriteLine("사용자의 반을 입력하세요");
-            info[0] = Console.ReadLine();
+            while (true)
+            {
+                info[0] = Console.ReadLine();
+                if (string.IsNullOrEmpty(info[0]))
+                {
+                    Console.WriteLine("정보를 입력하지 않았습니다. 다시 사용자의 반을 입력하세요.");
+                    continue;
+                }
+                break;
+            }
             Console.WriteLine("사용자의 번호을 입력하세요");
-            info[1] = Console.ReadLine();
+            while (true)
+            {
+                info[1] = Console.ReadLine();
+                if (string.IsNullOrEmpty(info[1]))
+                {
+                    Console.WriteLine("정보를 입력하지 않았습니다. 다시 사용자의 번호을 입력하세요.");
+                    continue;
+                }
+                break;
+            }
             return info;
         }
     }
